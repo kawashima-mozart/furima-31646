13b55b96e00cd64cc8bf060f1fff51b06e00f2a3
kawashima-mozart <nowhere.no.where.now.here.orz@gmail.com>
2020-11-11 20:34:00 +0900

    サインアップ、ログイン機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_paraneters, if: :devise_controller?
+  
+  private
+  def configure_permitted_paraneters
+    devise_parameter_sanitizer.permit(:sign_up,keys: [:nickname, :last_name_chn, :first_name_chn, :last_name_kana, :first_name_kana, :birthday]) 
+  end
 end
