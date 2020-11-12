a0f6e9592bf0f6c26084427db4d5ff09dc52b4d9
kawashima-mozart <nowhere.no.where.now.here.orz@gmail.com>
2020-11-12 17:09:32 +0900

    rubocop修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   with_options presence: true do
     validates :nickname
-    validates :password_confirmation 
-    validates :last_name_chn, format: {with: /\A[ぁ-んァ-ン一-龥]/, message:  "is invalid.全角（漢字・ひらがな・カタカナ）で入力してください"}
-    validates :first_name_chn, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid.全角（漢字・ひらがな・カタカナ）で入力してください"}
-    validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid.全角（カタカナ）で入力してください"}
-    validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid.全角（カタカナ）で入力してください"}
+    validates :password_confirmation
+    validates :last_name_chn, format: { with: /\A[ぁ-んァ-ン一-龥]/, message:  ' is invalid.' }
+    validates :first_name_chn, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: ' is invalid.' }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: ' is invalid.' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: ' is invalid.' }
     validates :birthday
   end
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i, message: 'is invalid.' }
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    email {Faker::Internet.free_email}
-    password {"test111"}
-    password_confirmation {password}
-    nickname {"tarou"}
-    last_name_chn {"亜アあ"}
-    first_name_chn {"亜アあ"}
-    last_name_kana {"ヤマダ"}
-    first_name_kana {"タロウ"}
-    birthday  {Faker::Date.between(from: '1930-01-01',to: Date.today)}
+    email { Faker::Internet.free_email }
+    password { 'test111' }
+    password_confirmation { password }
+    nickname { 'tarou' }
+    last_name_chn { '亜アあ' }
+    first_name_chn { '亜アあ' }
+    last_name_kana { 'ヤマダ' }
+    first_name_kana { 'タロウ' }
+    birthday { Faker::Date.between(from: '1930-01-01', to: Date.today) }
   end
 end
