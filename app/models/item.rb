e0b05b56192ca821bc0c4d08a3b8734cfb2249ce
kawashima-mozart <nowhere.no.where.now.here.orz@gmail.com>
2020-11-13 08:12:20 +0900

    各モデルのバリデーションと、各モデルとitemモデルとのアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping
+  belongs_to :shipment_source
+  belongs_to :day_of_shipment
+
+  with_options presence: true do
+    validates :category, numericality: { other_than: 1 }
+    validates :status, numericality: { other_than: 1 }
+    validates :shipping, numericality: { other_than: 1 }
+    validates :shipment_source, numericality: { other_than: 1 }
+    validates :day_of_shipment, numericality: { other_than: 1 }
+  end
 end
