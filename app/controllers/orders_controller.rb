ac9af4dc3a0aa0bcf3b154751febe5dec11da1d8
kawashima-mozart <nowhere.no.where.now.here.orz@gmail.com>
2020-11-17 01:19:14 +0900

    コントローラーの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,8 @@
 class OrdersController < ApplicationController
   before_action :sold_out_check
   before_action :move_to_index
+  before_action :orner_move_index
+
   def index
     @item_order = ItemOrder.new
   end
@@ -22,10 +24,6 @@ class OrdersController < ApplicationController
     params.require(:item_order).permit(:postal_code, :shipment_source_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
-  def move_to_index
-    redirect_to new_user_session_path unless user_signed_in?
-  end
-
   def pay_item
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
@@ -35,8 +33,16 @@ class OrdersController < ApplicationController
     )
   end
 
+  def orner_move_index
+    redirect_to root_path if @item.user.id == current_user.id
+  end
+
+  def move_to_index
+    redirect_to new_user_session_path unless user_signed_in?
+  end
+
   def sold_out_check
     @item = Item.find(params[:item_id])
-    redirect_to root_path if @item.order.present? || @item.user.id == current_user.id
+      redirect_to root_path if @item.order.present?
   end
 end
