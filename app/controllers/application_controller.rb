58372243c772a7c66aa1a2a9b6c6bf9dfef506c0
kawashima-mozart <nowhere.no.where.now.here.orz@gmail.com>
2020-11-17 11:18:57 +0900

    basic認証の導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_paraneters, if: :devise_controller?
 
   private
 
   def configure_permitted_paraneters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name_chn, :first_name_chn, :last_name_kana, :first_name_kana, :birthday])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username ==  ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
